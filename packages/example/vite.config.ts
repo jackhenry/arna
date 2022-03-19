76fbef5ff70f8c2ee7ff017a4fcbf66bdb93bf75
Jack Henry <jack@jackerickson.net>
2022-03-19 18:18:53 -0500

    Added rollup-plugin-analyzer to vite config. Moved some dependencies to devDependencies.

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -1,25 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 import preprocess from 'svelte-preprocess';
+import bundleAnalyzer from 'rollup-plugin-analyzer';
+import fs from 'fs';
 import postcss from './postcss.config';
 
 export default defineConfig({
   optimizeDeps: {
     include: [
-      // 'chroma-js',
-      // 'codemirror',
-      // 'konva',
-      // 'svelte',
-      // 'svelte-preprocess',
-      // '@sveltejs/vite-plugin-svelte',
+      '@arna/edge-bundler',
       '@arna/core',
       '@arna/renderer',
-      '@arna/edge-bundler',
     ],
   },
   build: {
     commonjsOptions: {
       include: [/edge-bundler/, /core/, /renderer/, /node_modules/],
+    },
+    rollupOptions: {
+      plugins: [bundleAnalyzer({
+        showExports: true,
+        writeTo: (analysisString: string) => {
+          fs.writeFileSync('./bundle-analysis.txt', analysisString);
+        },
+      })],
     },
   },
   plugins: [
