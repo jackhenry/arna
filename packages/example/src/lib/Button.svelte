928320349daa9782a26c806e0e9a9b73e9ba2ae3
Jack Henry <jack@jackerickson.net>
2022-03-19 21:29:22 -0500

    Refactored button component to use slot.

--- packages/example/src/lib/Button.svelte
+++ packages/example/src/lib/Button.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  export let label: string;
   export let onClick: () => void;
 </script>
 
 <button
   on:click={onClick}
   class={`rounded px-2 py-1 mx-1 text-override-fg text-sm font-semibold ${$$props.class}`}
-  >{label}</button
+  ><slot /></button
 >
