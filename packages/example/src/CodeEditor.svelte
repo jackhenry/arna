495422aa6f1de481907f3ad17ba2108bfad8f206
Jack Henry <jack@jackerickson.net>
2022-03-19 21:26:50 -0500

    Added image download functionality. Changed button layout to grid for better responsive experience.

--- packages/example/src/CodeEditor.svelte
+++ packages/example/src/CodeEditor.svelte
@@ -3,6 +3,7 @@
   import "codemirror/lib/codemirror.css";
   import "codemirror/theme/midnight.css";
   import { afterUpdate, onMount } from "svelte";
+  import CameraIcon from "./assets/CameraIcon.svelte";
   import createArnaMode from "./lib/arna_mode";
   import Button from "./lib/Button.svelte";
   import DropDown from "./lib/DropDown.svelte";
@@ -33,6 +34,24 @@
 
   const handleSubmit = () => {
     $code = editor.getValue();
+  };
+
+  const handleDownload = () => {
+    try {
+      const collection = document.getElementsByTagName("canvas");
+      if (collection.length < 0) {
+        throw new Error("Konva canvas is not loaded. Cannot create image.");
+      }
+      const canvas = collection.item(0);
+      const img = canvas.toDataURL("image/png");
+      const ghostLink = document.createElement("a");
+      ghostLink.setAttribute("href", img);
+      ghostLink.setAttribute("download", img);
+      ghostLink.click();
+      ghostLink.remove();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   onMount(() => {
@@ -67,26 +86,40 @@
   });
 </script>
 
-<div class="w-full h-22 mb-2">
+<div id="code-wrapper" class="w-full h-22 mb-2">
   <textarea bind:this={editorElement} id="editor" />
   <div class="flex flex-col items-center pt-2">
-    <div class="flex">
+    <div
+      class="grid grid-cols-custom gap-y-2 gap-x-2 min-w-full justify-items-center"
+    >
       <Button
         onClick={handleGenerateRandom}
-        label="Random"
         type="primary"
-        class="bg-override-blue hover:bg-override-blue/75 text-override-bg"
-      />
+        class="bg-override-blue hover:bg-override-blue/75 text-override-bg w-full"
+      >
+        <svelte:fragment>Random</svelte:fragment>
+      </Button>
       <Button
         onClick={handleRefresh}
         label="Refresh"
-        class="bg-override-yellow hover:bg-override-yellow/75 text-override-bg"
-      />
+        class="bg-override-yellow hover:bg-override-yellow/75 text-override-bg w-full"
+      >
+        <svelte:fragment>Refresh</svelte:fragment>
+      </Button>
       <Button
         onClick={handleSubmit}
         label="Submit"
-        class="bg-override-green hover:bg-override-green/75 text-override-bg"
-      />
+        class="bg-override-green hover:bg-override-green/75 text-override-bg w-full"
+      >
+        <svelte:fragment>Submit</svelte:fragment>
+      </Button>
+      <Button
+        onClick={handleDownload}
+        label="Download"
+        class="bg-override-purple hover:bg-override-purple/75 text-override-bg w-full max-h-[28px] flex items-center justify-center"
+      >
+        <CameraIcon class="!fill-override-neutral-900 mx-3" />
+      </Button>
     </div>
     <div
       class="flex flex-grow mt-2 cursor-pointer text-override-blue text-xs font-semibold"
