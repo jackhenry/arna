89e6c2ebaa16e0d872f4eda631c157d2bf9ebdb1
Jack Henry <jack@jackerickson.net>
2022-03-19 21:30:31 -0500

    Refactored Dropdown.svelte to use svg arrow instead of unicode arrows.

--- packages/example/src/lib/DropDown.svelte
+++ packages/example/src/lib/DropDown.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
+  import ArrowIcon from "../assets/ArrowIcon.svelte";
   import type { Writable } from "svelte/store";
+  import { onMount } from "svelte";
 
   export let text: string;
   export let show: Writable<boolean>;
+  let rootElement: HTMLDivElement;
+  let svgElement: SVGElement;
+
+  const updateRotation = () => {
+    if ($show) {
+      svgElement.classList.add("-rotate-90");
+    } else {
+      svgElement.classList.remove("-rotate-90");
+    }
+  };
+
+  onMount(() => {
+    updateRotation();
+    rootElement.addEventListener("click", () => {
+      $show = !$show;
+      updateRotation();
+    });
+  });
 </script>
 
-<div
-  on:click={() => {
-    $show = !$show;
-  }}
-  class={`${$$props.class}`}
->
+<div bind:this={rootElement} class={`flex items-center ${$$props.class}`}>
   <span class="underline hover:text-override-fg focus:text-override-neutral-200"
     >{text}</span
   >
-  <span class="text-override-yellow">{$show ? "▼" : "►"}</span>
+  <ArrowIcon bind:svgElement class="!fill-override-yellow transition" />
 </div>
