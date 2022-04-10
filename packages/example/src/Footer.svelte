b0ef4d0d39385481682e257b603f40037203b539
Jack Henry <jack@jackerickson.net>
2022-04-10 19:28:36 +0000

    Fixed typo.

--- packages/example/src/Footer.svelte
+++ packages/example/src/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="w-full mb-2">
   <div class="flex text-override-blue font-medium text-xs md:text-sm font-sans">
-    <span class="">mırzam</span>
+    <span class="">arna</span>
     <a
       href="https://jackhenry.github.io/"
       class="flex-grow text-override-yellow text-center"
--- packages/example/src/NavBar.svelte
+++ packages/example/src/NavBar.svelte
@@ -5,9 +5,8 @@
 <div class="text-override-bg overflow-hidden bg-override-yellow relative p-1">
   <div class="w-full flex">
     <div class="flex-grow">
-      <span class="font-semibold">күнім</span>
     </div>
-    <div class="">
+    <div>
       <a href="https://github.com/jackhenry/arna"
         ><GithubIcon
           class="!fill-override-bg/80 hover:!fill-override-blue w-auto h-7"
